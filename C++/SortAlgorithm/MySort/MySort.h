83cde2c9d75ed275281a1fdebb2912e2faac9f60
Cherryzc <1147336656@qq.com>
2024-04-13 14:08:06 +0800

    Sort

--- C++/SortAlgorithm/MySort/MySort.h
+++ C++/SortAlgorithm/MySort/MySort.h
@@ -8,6 +8,11 @@
 // TODO: 在此处引用程序需要的其他标头。
 void PrintArr(int* arr, int nSize);
 
+
+// std::sort 基于我们熟悉的insertion sort, heap sort, quick sort. 1.检查容器元素数量，小于32则为插入排序, 2.当divisions数量达到限制（1.5 log2(N)）时，使用堆排序。3.否则，使用快速排序进行划分和部分递归。
+// std::stable_sort 检查元素数量，小于32则为插入排序,如果大于32则进行归并排序
+// std::partial_sort 可以对序列进行部分排序。它使用快速排序或堆排序，平均时间复杂度为O(n log n)。注意，std::partial_sort() 不保证稳定性。
+
 void BubbleSort(int *arr, int nSize);
 
 void SelectSort(int* arr, int nSize);
